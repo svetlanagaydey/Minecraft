27738d8e0c181291faffa2e140d4a45694409f6f
svetlanagaydey <lana84@ukr.net>
2021-12-03 09:09:15 +0200

    matrix created"

--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
-const elements = {
-    cloud = "cloud",
-    treeTrunk = "treeTrunk",
-    treeLeaves = "treeLeaves",
-    stone = "stone",
-    ground = "ground"
+const field = document.querySelector('.playGraund');
+
+for (let i=1; i<=20; i++) {
+    for (let j=1; j<=20; j++) {
+        const playBlock = document.createElement('div');
+        playBlock.id = `x${j}y${i}`;
+        playBlock.classList.add('playItem');
+        field.appendChild(playBlock);
+    }
+}
+
+createSmallCloud(2, 3);
+createBidCloud(12, 3)
+create
+
+function createSmallCloud(startX, startY) {
+    document.querySelector(`#x${startX}y${startY}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+1}y${startY}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+1}y${startY+1}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+2}y${startY+1}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+2}y${startY}`).setAttribute('class', 'cloud');
+}
+
+function createBidCloud(startX, startY) {
+    document.querySelector(`#x${startX}y${startY}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+5}y${startY}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+6}y${startY}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+7}y${startY}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+1}y${startY+1}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+2}y${startY+1}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+3}y${startY+1}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+4}y${startY+1}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+5}y${startY+1}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+3}y${startY+2}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+4}y${startY+2}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+5}y${startY+2}`).setAttribute('class', 'cloud');
+    document.querySelector(`#x${startX+6}y${startY+2}`).setAttribute('class', 'cloud');
 }
